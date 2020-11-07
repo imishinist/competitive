80dbb35b8de0ea8b33dcfff3bb4d9a911de0b0c4
Taisuke Miyazaki <imishinist@gmail.com>
2020-11-08 07:35:19 +0900

    impl abc181 d in smart algorithm

--- abc181/e/main.cpp
+++ abc181/e/main.cpp
@@ -21,34 +21,24 @@ int main() {
     sort(H.begin(), H.end());
     sort(W.begin(), W.end());
 
+    vector<int> sum1((n + 1) / 2);
+    vector<int> sum2((n + 1) / 2);
+
+    for (int i = 0; i + 1 < n; i += 2) {
+        int j = i + 1;
+        sum1[i / 2 + 1] = sum1[i / 2] + H[i+1] - H[i];
+        sum2[j / 2 + 1] = sum2[j / 2] + H[j+1] - H[j];
+    }
+
     long long ans = 100000000000;
     for (auto w : W) {
         int idx = lower_bound(H.begin(), H.end(), w) - H.begin();
         long long sum = 0;
-
-        if (idx == 0) {
-            sum += H[idx] - w;
-            for (int i = 1; i < n - 1; i += 2) {
-                sum += H[i+1] - H[i];
-            }
-        } else if (idx == n) {
+        if (idx % 2 != 0) {
             idx--;
-            sum += w - H[idx];
-            for (int i = 0; i < n - 1; i += 2) {
-                sum += H[i+1] - H[i];
-            }
-        } else {
-            if (idx % 2 != 0) {
-                idx--;
-            }
-            for (int i = 0; i < idx; i += 2) {
-                sum += H[i+1] - H[i];
-            }
-            sum += abs(H[idx] - w);
-            for (int i = idx+1; i < n - 1; i += 2) {
-                sum += H[i+1] - H[i];
-            }
         }
+        sum += abs(H[idx] - w);
+        sum += sum1[idx / 2] + sum2[n / 2] - sum2[idx / 2];
         ans = min(ans, sum);
     }
     printf("%lld\n", ans);    
