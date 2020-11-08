1516c3f463dbfff829030bb76f260cba468405a6
Taisuke Miyazaki <imishinist@gmail.com>
2020-11-09 00:27:21 +0900

    impl abc180 b

--- abc180/b/main.cpp
+++ abc180/b/main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <map>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 
@@ -12,5 +13,20 @@ typedef vector<pair<int, int>> vpii;
 #define eps 1e-14
 
 int main() {
+    int n;
+    cin >> n;
+
+    long long m = 0;
+    long long e = 0;
+    long long c = 0;
+    for (int i = 0; i < n; i++) {
+        long long x;
+        cin >> x;
+        m += abs(x);
+        e += abs(x) * (long long)abs(x);
+        c = max(c, abs(x));
+    }
+
+    printf("%lld\n%.15lf\n%lld\n", m, sqrt(e), c);
     return 0;
 }
