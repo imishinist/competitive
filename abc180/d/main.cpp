e26f772d7c4cfbda9ab126868c575ad36f0ffc4f
Taisuke Miyazaki <imishinist@gmail.com>
2020-11-09 02:15:09 +0900

    impl abc180 d

--- abc180/d/main.cpp
+++ abc180/d/main.cpp
@@ -8,9 +8,23 @@ using namespace std;
 
 typedef vector<int> vi;
 typedef vector<pair<int, int>> vpii;
+typedef long long ll;
 
 #define eps 1e-14
 
 int main() {
+    ll x, y, a, b;
+    cin >> x >> y >> a >> b;
+
+    ll ans = 0;
+    while ((double) a*x <= 2e18) {
+        if (a * x >= y || a * x > x + b) {
+            break;
+        }
+        ans++;
+        x *= a;
+    }
+    ans += (y - x - 1) / b;
+    cout << ans << endl;
     return 0;
 }
