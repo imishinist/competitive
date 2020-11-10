95822ae666a1b566b2460bf7adfb7f117a685edf
Taisuke Miyazaki <imishinist@gmail.com>
2020-11-10 18:11:07 +0900

    impl abc182 b

--- abc182/b/main.cpp
+++ abc182/b/main.cpp
@@ -13,5 +13,24 @@ typedef vector<pair<int, int>> vpii;
 #define eps 1e-14
 
 int main() {
+    int n;
+    cin >> n;
+
+    vi A(n);
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+    }
+
+    vector<int> table(1000);
+    for (int i = 2; i <= 1000; i++) {
+        for (int a : A) {
+            if (a % i == 0) {
+                table[i]++;
+            }
+        }
+    }
+
+    auto it = max_element(table.begin(), table.end());
+    cout << it - table.begin() << endl;
     return 0;
 }
