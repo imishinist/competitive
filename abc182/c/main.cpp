15167b13a71264a4eb868233fc0714a6cfd6f954
Taisuke Miyazaki <imishinist@gmail.com>
2020-11-10 19:40:31 +0900

    impl abc182 c

--- abc182/c/main.cpp
+++ abc182/c/main.cpp
@@ -12,6 +12,33 @@ typedef vector<pair<int, int>> vpii;
 
 #define eps 1e-14
 
+bool is_3mul(string n) {
+    int sum = 0;
+    for (int i = 0; i < n.size(); i++) {
+        sum += n[i] - '0';
+    }
+    return sum % 3 == 0;
+}
+
 int main() {
+    string n;
+    cin >> n;
+
+    int ans = 20;
+    for (int i = 0; i < (1 << n.size()) - 1; i++) {
+        int cnt = n.size();
+        string cp_n = "";
+        for (int j = 0; j < n.size(); j++) {
+            if (!((i >> j) & 1)) {
+                cnt--;
+                cp_n = cp_n + n[j];
+            }
+        }
+        if (is_3mul(cp_n)) {
+            ans = min(ans, cnt);
+        }
+    }
+    cout << ((ans != 20) ? ans : -1) << endl;
+
     return 0;
 }
