8e779ffde1b1894fb3dc34b46271a689d12ed9ff
Taisuke Miyazaki <imishinist@gmail.com>
2020-11-11 02:45:01 +0900

    impl abc182 d

--- abc182/d/main.cpp
+++ abc182/d/main.cpp
@@ -9,9 +9,26 @@ using namespace std;
 
 typedef vector<int> vi;
 typedef vector<pair<int, int>> vpii;
+typedef long long ll;
 
 #define eps 1e-14
 
 int main() {
+    long long n;
+    cin >> n;
+    vector<ll> A(n);
+    ll sum = 0, num = 0, max = 0, ans = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+        num += A[i];
+        if (max < num) {
+            max = num;
+        }
+        if (ans < sum + max)
+            ans = sum + max;
+        sum += num;
+    }
+    cout << ans << endl;
+
     return 0;
 }
