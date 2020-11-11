9113e593173221f06f4920d77310359487018f9d
Taisuke Miyazaki <imishinist@gmail.com>
2020-11-12 03:55:14 +0900

    impl abc178 b

--- abc178/b/main.cpp
+++ abc178/b/main.cpp
@@ -9,9 +9,13 @@ using namespace std;
 
 typedef vector<int> vi;
 typedef vector<pair<int, int>> vpii;
+using ll = long long;
 
 #define eps 1e-14
 
 int main() {
+    ll a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << max(max(a * c, a * d), max(b * c, b * d)) << endl;
     return 0;
 }
