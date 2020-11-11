cb7828940a60e5fe4482d11603de384b9b4c742b
Taisuke Miyazaki <imishinist@gmail.com>
2020-11-12 03:43:40 +0900

    impl abc178 a

---
+++ abc178/a/main.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<pair<int, int>> vpii;
+
+#define eps 1e-14
+
+int main() {
+    int x;
+    cin >> x;
+    printf("%d\n", x ^ 1);
+    return 0;
+}
---
+++ abc178/c/main.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<pair<int, int>> vpii;
+
+#define eps 1e-14
+
+int main() {
+    return 0;
+}
