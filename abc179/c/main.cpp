d4b828b34adc11aea707557c997416110c6967d4
Taisuke Miyazaki <imishinist@gmail.com>
2020-11-11 12:41:25 +0900

    impl abc179 c

--- abc179/c/main.cpp
+++ abc179/c/main.cpp
@@ -10,8 +10,26 @@ using namespace std;
 typedef vector<int> vi;
 typedef vector<pair<int, int>> vpii;
 
+typedef long long ll;
+
 #define eps 1e-14
 
 int main() {
+    int n;
+    cin >> n;
+    ll ans = 0;
+    for (int i = 1; i < n; i++) {
+        int tmp = n - i;
+        for (int j = 1; j * j <= tmp; j++) {
+            if (tmp % j == 0) {
+                if (tmp == j * j) {
+                    ans++;
+                    continue;
+                }
+                ans += 2;
+            }
+        }
+    }
+    cout << ans << endl;
     return 0;
 }
