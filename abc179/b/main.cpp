f3593afbcd72348af7858b0243d5f2eac126934a
Taisuke Miyazaki <imishinist@gmail.com>
2020-11-11 12:03:41 +0900

    impl abc179 b

--- abc179/b/main.cpp
+++ abc179/b/main.cpp
@@ -13,5 +13,25 @@ typedef vector<pair<int, int>> vpii;
 #define eps 1e-14
 
 int main() {
+    int n;
+    cin >> n;
+    vpii d(n);
+
+    bool ans = 0;
+    int cnt = 0;
+    for (int i = 0; i < n; i++) {
+        int d1, d2;
+        cin >> d1 >> d2;
+
+        if (d1 == d2) {
+            cnt++;
+        } else {
+            cnt = 0;
+        }
+
+        ans |= (cnt >= 3);
+    }
+    cout << (ans ? "Yes" : "No") << endl;
+
     return 0;
 }
