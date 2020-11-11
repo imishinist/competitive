c8b09b5e39f2dc5318af33a36b5800d7db4e1d2f
Taisuke Miyazaki <imishinist@gmail.com>
2020-11-11 12:00:36 +0900

    impl abc179 a

--- abc179/a/main.cpp
+++ abc179/a/main.cpp
@@ -13,5 +13,13 @@ typedef vector<pair<int, int>> vpii;
 #define eps 1e-14
 
 int main() {
+    string s;
+    cin >> s;
+    cout << s;
+    if (s[s.size() - 1] == 's') {
+        cout << "es" << endl;
+    } else {
+        cout << "s" << endl;
+    }
     return 0;
 }
